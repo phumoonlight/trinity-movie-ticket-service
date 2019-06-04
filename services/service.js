839595b29236fa6356648fb152ee803eb4e8c3d9
Phusan Moonmuang <phumoonlight@hotmail.com>
2019-06-04 13:55:40 +0700

    add cors

--- services/service.js
+++ services/service.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import movies from '../controllers/movies';
+
 
 mongoose.connect('mongodb://localhost/mydb', { useNewUrlParser: true });
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
